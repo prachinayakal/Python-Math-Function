d37f7de93fb0ce8eed26e0ccaf22419b0cf49292
Prachi Nayakal <121372605+prachinayakal@users.noreply.github.com>
2024-10-27 20:51:46 +0530

    Add files via upload

---
+++ math_function.py
@@ -0,0 +1,29 @@
+# Build-in Mathematical functions
+
+# max
+# max function is used to find the maximum value from the given values
+val = [10,50,46,98]
+print(max(val))
+
+# min
+# min function is used to find the minimum value from the given values
+print(min(val))
+
+# abs
+# abs function is used to find the abstract value
+val2= -25
+print(abs(val2))
+
+# pow
+# The pow() function returns x raised to y. It is equivalent to x**y
+print(pow(2,4))
+
+# round
+# the round function is used to calculate the round figure value
+val3=12.6
+print(round(val3))
+
+# sum
+# the sum function is used to do the sum of the numbers 
+s=[1,2,4]
+print(sum(s))
